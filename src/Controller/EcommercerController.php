e8f4c007986851316262078a495581c6e2cff114
sam <smass59260@gmail.com>
2022-03-30 09:50:11 +0200

    avant migration

---
+++ src/Controller/EcommercerController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EcommercerController extends AbstractController
+{
+    /**
+     * @Route("/ecommerce", name="app_ecommerce")
+     */
+    public function index(): Response
+    {
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+        $articles = $repo->findAll();
+
+        return $this->render('ecommercer/index.html.twig', [
+            'articles' => $articles,
+        ]);
+    }
+}
